df91d5e9f41913af13746225724e66f9e9fed6ce
LordTuxn <r3tuxn@gmail.com>
2021-09-16 17:49:18 +0200

    Fixed bug that caused some messages not to be broadcast

---
+++ src/main/java/com/alpsbte/bungee/ChatHandler.java
@@ -0,0 +1,39 @@
+package com.alpsbte.Bungee;
+
+import net.md_5.bungee.api.config.ServerInfo;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.logging.Level;
+
+public class ChatHandler extends Thread {
+    protected final Socket socket;
+
+    public ChatHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try (InputStream input = socket.getInputStream()) {
+            ObjectInputStream objectInput = new ObjectInputStream(input);
+            String message = (String) objectInput.readObject();
+            String playerServer = (String) objectInput.readObject();
+
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            DataOutputStream out = new DataOutputStream(stream);
+            out.writeUTF(message);
+
+            for(ServerInfo server : BungeePlugin.getPlugin().getProxy().getServers().values()) {
+                if(!server.getName().equalsIgnoreCase(playerServer) && !server.getPlayers().isEmpty()) {
+                    server.sendData("alpsbte:globalchat", stream.toByteArray());
+                }
+            }
+
+            stream.close();
+            out.close();
+            socket.close();
+        } catch (Exception ex) {
+            BungeePlugin.getPlugin().getLogger().log(Level.SEVERE, "Could not handle socket message from server!", ex);
+        }
+    }
+}
